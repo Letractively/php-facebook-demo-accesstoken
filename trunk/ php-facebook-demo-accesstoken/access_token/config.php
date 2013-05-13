01b473ac5e01b814ebc7571975b6bac6ce2c21aa
mrthanh223@gmail.com <mrthanh223@gmail.com@c92b748d-cb70-4764-e45b-5848ab3abda3>
2013-05-13 15:41:05 +0000

    config.php : thêm $facebook = new Facebook($config) Thêm thông tin cho phần index + mấy phần kia

--- trunk/ php-facebook-demo-accesstoken/access_token/config.php
+++ trunk/ php-facebook-demo-accesstoken/access_token/config.php
@@ -1,9 +1,12 @@
 <?php
 require_once '../src/facebook.php';
-$facebook = new Facebook(array
-            (
-            'appId' => '539935456045933',
-            'secret' => 'b8e72a6e4deba0ad85dd803074b47d27',
-        ));
 
+
+$config = array(); 
+$config["appId"] = '539935456045933'; 
+$config["secret"] = 'b8e72a6e4deba0ad85dd803074b47d27'; 
+
+$facebook = new Facebook($config);
+ 
+ $me = $facebook->api('/me');
 ?>
