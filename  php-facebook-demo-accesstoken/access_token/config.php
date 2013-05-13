ce5854e418069eec8dbf11aabf73220fca158f7c
mrthanh223 <mrthanh223@gmail.com>
2013-05-13 03:22:56 +0000

    chinh sua

---  php-facebook-demo-accesstoken/access_token/config.php
+++  php-facebook-demo-accesstoken/access_token/config.php
@@ -1,3 +1,10 @@
 <?php
-
+require_once '../src/facebook.php';
+$facebook = new Facebook(array
+            (
+            'appId' => '539935456045933',
+            'secret' => 'b8e72a6e4deba0ad85dd803074b47d27',
+        ));
+$user_id = $facebook->getUser();
+$me = $facebook->api('/me');
 ?>
